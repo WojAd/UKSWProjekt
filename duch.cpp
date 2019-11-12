d75283afeb1625378dc7f5fff670f14986d69ac8
WojAd <adraxer@interia.pl>
2019-11-12 12:57:05 +0100

    zmiana

---
+++ duch.cpp
@@ -0,0 +1,43 @@
+#include "duch.h"
+
+
+Ghost::Ghost(char* pacmanX, char* pacmanY, char map[sizeMapY][sizeMapX]) {
+	this->pacmanX = pacmanX;
+	this->pacmanY = pacmanY;
+	for (short i = 0; i < sizeMapY; i++) {
+		for (short j = 0; j < sizeMapX; j++) {
+			this->map[i][j] = map[i][j];
+		}
+	}
+	thread aStar(this->si);
+}
+
+Ghost::~Ghost() {
+	deWay.clear();
+}
+
+void Ghost::si() {
+	while (1) {
+		if (pacmanX == NULL || pacmanY == NULL) { continue; }
+		deque<Node> result;
+		deque<Node> map;
+		for (int i = 0; i < sizeMapY; i++) {
+			for (int j = 0; j < sizeMapX; j++) {
+				if (this->map[i][j] == ' ') {
+					map.push_back(Node(i, j));
+				}
+			}
+		}
+		Node destination;
+
+		/* A* path find algorithm */
+		
+		deque<Node> closed;
+		deque<Node> open;
+
+		/*________________________*/
+
+		deWay.clear();
+		for (Node q : result) deWay.push_back(q);
+	}
+}
---
+++ duch.h
@@ -0,0 +1,42 @@
+#ifndef GHOST_H
+#define GHOST_H
+
+using namespace std;
+
+#include <SFML/Graphics.hpp>
+#include <thread>
+#include <deque>
+
+#define sizeMapX 20
+#define sizeMapY 20
+
+class Node {
+public:
+	short X;
+	short Y;
+	Node* parent;
+
+	Node() { X = 0; Y = 0; parent = NULL; }
+	Node(short X, short Y) { this->X = X; this->Y = Y; parent = NULL; }
+	~Node() {};
+
+	bool isNeighbour(Node param) { if (param.X >= X - 1 || X >= param.X - 1 || param.Y >= Y - 1 || Y >= param.Y - 1) return true; else return false; }
+	short distance(Node param) { return sqrt(pow(abs(param.X - X), 2) + pow(abs(param.Y - Y), 2)); }
+};
+
+class Ghost {
+	char* pacmanX;
+	char* pacmanY;
+	char map[sizeMapX][sizeMapY];
+	std::deque<Node> deWay;
+	
+public:
+	sf::Sprite sprite;
+
+	Ghost(char* pacmanX, char* pacmanY, char map[sizeMapY][sizeMapX]);
+	~Ghost();
+
+	void si();
+};
+
+#endif //GHOST_H
