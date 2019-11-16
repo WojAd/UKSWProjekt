11ef929520aa0144c70038884fdbac41a7b1a3c9
SwRafal <juniorekrafcio@interia.pl>
2019-11-16 15:42:45 +0100

    Aktualizacja Game i main oraz mapy. Dodano plik ze ścieżkami do zasobów.

---
+++ FilePaths.h
@@ -0,0 +1,6 @@
+#ifndef FILEPATHS_H
+#define FILEPATHS_H
+
+#define TEXPATH_TILES "resources/textures/tiles.png"
+
+#endif
--- mapa.h
+++ mapa.h
@@ -9,17 +9,27 @@
 #define TILE_WIDTH 50
 #define TILE_HEIGHT 50
 
-class Mapa : public sf::Drawable
+class Mapa
 {
 private:
 	char _map[MAP_WIDTH][MAP_HEIGHT];
 	sf::Sprite _tile;//For drawing
 
+	//Tiles order in texture
+	const int t_corridor = 0;
+	const int t_cross = 1;
+	const int t_vertical = 2;
+	const int t_horizontal = 3;
+	const int t_left = 4;
+	const int t_right = 5;
+	const int t_up = 6;
+	const int t_down = 7;
+
 public:
-	Mapa();
+	Mapa(sf::Texture &_tiles_tex);
 	~Mapa();
 
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	void draw(sf::RenderWindow &win);
 
 	void fillMap(char type);
 	void setTile(unsigned int x, unsigned int y, char type);
