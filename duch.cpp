4e55c6880aeeec8ec183343fc689aa708486ae8c
WojAd <adraxer@interia.pl>
2020-01-07 13:26:10 +0100

    Duszki próba optymalizacji. Przy 60 FPS spada mi do 59. Przy 120 FPS spada mi do 103

--- duch.cpp
+++ duch.cpp
@@ -17,7 +17,7 @@ Ghost::Ghost(int x, int y, Mapa* map) {
 	threads = NULL;
 	for (int y = 0; y < MAP_HEIGHT; y++)
 		for (int x = 0; x < MAP_WIDTH; x++) {
-			if (map->getTile(x,y) == CORRIDOR)
+			if (map->getTile(x, y) == CORRIDOR)
 				this->map.push_back(Node(x, y));
 		}
 }
@@ -29,8 +29,8 @@ Ghost::~Ghost() {
 void Ghost::update(float destXParam, float destYParam)
 {
 	sf::Vector2u tile = mapPointer->pixelsToTilecoords(sf::Vector2f(destXParam, destYParam));
-	destX = (destXParam-132)/40;
-	destY = (destYParam)/40;
+	destX = (destXParam - 132) / 40;
+	destY = (destYParam) / 40;
 	if (threading == false && newDeWay.empty()) {
 		if (threads != NULL && !threads->joinable()) {
 			delete threads;
@@ -42,7 +42,7 @@ void Ghost::update(float destXParam, float destYParam)
 		threads = new thread(&Ghost::si, this);
 	}
 	if (!newDeWay.empty() && threading == false) {
-		if (!deWay.empty() && newDeWay.size() - deWay.size() / 4 >= deWay.size() && equal(deWay.begin(), deWay.end()-deWay.size()/4, newDeWay.begin())) newDeWay.clear();
+		if (!deWay.empty() && newDeWay.size() - deWay.size() / 4 >= deWay.size() && equal(deWay.begin(), deWay.end() - deWay.size() / 4, newDeWay.begin())) newDeWay.clear();
 		else {
 			sf::Vector2u tile = mapPointer->pixelsToTilecoords(sf::Vector2f(getX(), getY()));
 			short currTileX = tile.x;
@@ -54,16 +54,6 @@ void Ghost::update(float destXParam, float destYParam)
 					break;
 				}
 			}
-			/*if (!currNode.isNeighbour(newDeWay.back()) && currNode.X != newDeWay.back().X && currNode.Y != newDeWay.back().Y) {
-				Node candidate;
-				for (deque<Node>::iterator i = map.begin(); i < map.end(); i++) {
-					if ((currNode.X == i->X && newDeWay.back().Y == i->Y) || (newDeWay.back().X == i->X && currNode.Y == i->Y)) {
-						candidate = *i;
-						break;
-					}
-				}
-				newDeWay.push_back(candidate);
-			}*/
 			deWay = newDeWay;
 			newDeWay.clear();
 		}
@@ -90,12 +80,19 @@ void Ghost::update(float destXParam, float destYParam)
 		short nextPixelY = nextTileY * 40 + 20;
 		Node currNode(currTileX, currTileY);
 		Node nextNode(nextTileX, nextTileY);
-		
-		if (currPixelX == nextPixelX && currPixelY == nextPixelY) deWay.pop_back(	);
-		else if (currPixelX < nextPixelX) this->sprite.move(sf::Vector2f(ghostVelocity, 0));
-		else if (currPixelX > nextPixelX) this->sprite.move(sf::Vector2f(-ghostVelocity, 0));
-		else if (currPixelY < nextPixelY) this->sprite.move(sf::Vector2f(0, ghostVelocity));
-		else if (currPixelY > nextPixelY) this->sprite.move(sf::Vector2f(0, -ghostVelocity));
+		if (abs(currTileX - nextTileX) <= 1 && abs(currTileY - nextTileY <= 1) && mapPointer->getTile(nextTileX, nextTileY) == CORRIDOR) {
+			if (currPixelX == nextPixelX && currPixelY == nextPixelY) deWay.pop_back();
+			else if (currPixelX < nextPixelX) this->sprite.move(sf::Vector2f(ghostVelocity, 0));
+			else if (currPixelX > nextPixelX) this->sprite.move(sf::Vector2f(-ghostVelocity, 0));
+			else if (currPixelY < nextPixelY) this->sprite.move(sf::Vector2f(0, ghostVelocity));
+			else if (currPixelY > nextPixelY) this->sprite.move(sf::Vector2f(0, -ghostVelocity));
+		}
+		else {
+			if (currPixelY < 40*currTileY+20) this->sprite.move(sf::Vector2f(0, ghostVelocity));
+			else if (currPixelY > 40 * currTileY + 20) this->sprite.move(sf::Vector2f(0, -ghostVelocity));
+			else if (currPixelX < 40 * currTileX + 152) this->sprite.move(sf::Vector2f(ghostVelocity, 0));
+			else if (currPixelX > 40 * currTileX + 152) this->sprite.move(sf::Vector2f(-ghostVelocity, 0));
+		}
 	}
 }
 
@@ -105,9 +102,8 @@ void Ghost::draw(RenderTarget& target, RenderStates state) const
 }
 
 void Ghost::si() {
-
-	short x = (getX() - 132) / 40;
-	short y = (getY()) / 40;
+	short x = (getX() - 152) / 40;
+	short y = (getY() - 20) / 40;
 	//destX, destY
 	Node destination(destX, destY, 0, NULL);
 	//deque<Node> map
@@ -139,8 +135,13 @@ void Ghost::si() {
 			for (deque<Node>::iterator i = open.begin(); i < open.end(); i++) {
 				if (minimum->dCost + minimum->getDistance(destination) > i->dCost + i->getDistance(destination)) minimum = i;
 			}
-			if (closed.size() > 200) {
-				Sleep(100);
+			if (closed.size() > 150) {
+				Node* q = &closed.back();
+				newDeWay.push_back(*q);
+				while (q->parent != NULL) {
+					q = q->parent;
+					newDeWay.push_back(*q);
+				}
 				threading = false;
 				return;
 			}
@@ -155,7 +156,6 @@ void Ghost::si() {
 		}
 	}
 	threading = false;
-	Sleep(500);
 }
 
 Sprite Ghost::getSprite() const {
